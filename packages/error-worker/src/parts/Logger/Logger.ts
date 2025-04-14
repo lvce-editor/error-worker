c8074da9ee2f62a8daedf7beb94628bc50778d57
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-14 19:35:04 +0200

    feature: add logger (#19)

---
+++ packages/error-worker/src/parts/Logger/Logger.ts
@@ -0,0 +1,11 @@
+export const info = (...args: readonly any[]) => {
+  console.info(...args)
+}
+
+export const warn = (...args: readonly any[]) => {
+  console.warn(...args)
+}
+
+export const error = (...args: readonly any[]) => {
+  console.error(...args)
+}
