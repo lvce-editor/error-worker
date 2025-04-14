6f4e35e5863c08a48c0b755470f30f662a718b34
Le Vivilet <levivlet@pm.me>
2025-04-14 18:17:00 +0200

    feature: simplify eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,3 @@
 import * as config from '@lvce-editor/eslint-config'
 
-export default [
-  ...config.default,
-  {
-    ignores: ['packages/text-search-worker/src/textSearchWorkerMain.ts'],
-  },
-]
+export default [...config.default]
