2d4b54973110218341e251b16cb4769944555329
Le Vivilet <levivlet@pm.me>
2025-04-15 22:41:35 +0200

    feature: run lint in ci

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,16 @@
 import * as config from '@lvce-editor/eslint-config'
 
-export default [...config.default]
+export default [
+  ...config.default,
+  {
+    rules: {
+      'unicorn/prefer-spread': 'off',
+
+      // TODO
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      'unicorn/numeric-separators-style': 'off',
+      'no-console': 'off',
+      'no-useless-escape': 'off',
+    },
+  },
+]
