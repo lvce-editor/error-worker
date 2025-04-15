1367871c2faab01dec69201f77d51b945065928e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-15 21:50:38 +0200

    feature: register prettyError commands (#21)
    * feature: register prettyError commands
    
    * memory

--- packages/error-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/error-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,8 +1,11 @@
 import * as Ajax from '../Ajax/Ajax.ts'
 import * as Terminate from '../Terminate/Terminate.ts'
+import * as PrettyError from '../PrettyError/PrettyError.ts'
 
 export const commandMap = {
   'Errors.getJson': Ajax.getJson,
   'Errors.getText': Ajax.getText,
   'Errors.terminate': Terminate.terminate,
+  'Errors.prepare': PrettyError.prepare,
+  'Errors.print': PrettyError.print,
 }
