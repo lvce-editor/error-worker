615a8e7d05b3ef98ae13258925d23edbdf515aa0
Le Vivilet <levivlet@pm.me>
2025-07-30 16:23:36 +0200

    feature: register errormessage command

--- packages/error-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/error-worker/src/parts/CommandMap/CommandMap.ts
@@ -2,6 +2,7 @@ import * as Ajax from '../Ajax/Ajax.ts'
 import * as HandleMessagePort from '../HandleMessagePort/HandleMessagePort.ts'
 import * as PrettyError from '../PrettyError/PrettyError.ts'
 import * as Terminate from '../Terminate/Terminate.ts'
+import { tryToGetActualImportErrorMessage } from '../TryToGetActualImportErrorMessage/TryToGetActualImportErrorMessage.ts'
 
 export const commandMap = {
   'Errors.getJson': Ajax.getJson,
@@ -10,4 +11,5 @@ export const commandMap = {
   'Errors.prepare': PrettyError.prepare,
   'Errors.print': PrettyError.print,
   'Errors.terminate': Terminate.terminate,
+  'Errors.tryToGetActualImportErrorMessage': tryToGetActualImportErrorMessage,
 }
