039cbb82b119d8e85356e8a3879e7e46d5692170
Le Vivilet <levivlet@pm.me>
2025-07-30 16:20:23 +0200

    feature: update error worker path in postinstall

--- packages/build/src/build-static.js
+++ packages/build/src/build-static.js
@@ -1,4 +1,4 @@
-import { cp } from 'node:fs/promises'
+import { cp, readFile, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { pathToFileURL } from 'node:url'
 import { root } from './root.js'
@@ -10,14 +10,29 @@ const sharedProcessUrl = pathToFileURL(sharedProcessPath).toString()
 const sharedProcess = await import(sharedProcessUrl)
 
 process.env.PATH_PREFIX = '/error-worker'
-await sharedProcess.exportStatic({
+const { commitHash } = await sharedProcess.exportStatic({
   root,
   extensionPath: '',
 })
+
+const rendererWorkerPath = join(root, 'dist', commitHash, 'packages', 'renderer-worker', 'dist', 'rendererWorkerMain.js')
 
 export const getRemoteUrl = (path) => {
   const url = pathToFileURL(path).toString().slice(8)
   return `/remote/${url}`
 }
+
+const content = await readFile(rendererWorkerPath, 'utf8')
+const workerPath = join(root, '.tmp/dist/dist/errorWorkerMain.js')
+const remoteUrl = getRemoteUrl(workerPath)
+
+const occurrence = `// const errorWorkerUrl = \`\${assetDir}/packages/error-worker/dist/errorWorkerMain.js\`
+const errorWorkerUrl = \`${remoteUrl}\``
+const replacement = `const errorWorkerUrl = \`\${assetDir}/packages/error-worker/dist/errorWorkerMain.js\``
+if (!content.includes(occurrence)) {
+  throw new Error('occurrence not found')
+}
+const newContent = content.replace(occurrence, replacement)
+await writeFile(rendererWorkerPath, newContent)
 
 await cp(join(root, 'dist'), join(root, '.tmp', 'static'), { recursive: true })
