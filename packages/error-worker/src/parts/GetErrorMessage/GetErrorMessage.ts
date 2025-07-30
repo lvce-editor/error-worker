cfbd7eedc3367d74f9827d0a0458cf724d959396
Le Vivilet <levivlet@pm.me>
2025-07-30 16:20:40 +0200

    feature: format code

--- packages/error-worker/src/parts/GetErrorMessage/GetErrorMessage.ts
+++ packages/error-worker/src/parts/GetErrorMessage/GetErrorMessage.ts
@@ -2,7 +2,7 @@ export const getErrorMessage = (error: any): string => {
   if (!error) {
     return `Error: ${error}`
   }
-  let {message} = error
+  let { message } = error
   while (error.cause) {
     error = error.cause
     message += `: ${error}`
