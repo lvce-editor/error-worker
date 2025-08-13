e690145627ab0fe1f469a54e5f8434f494eac0a2
Le Vivilet <levivlet@pm.me>
2025-08-13 14:57:58 +0200

    measure

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -1,7 +1,7 @@
 import { join } from 'node:path'
 import { root } from './root.ts'
 
-export const threshold = 1_500_000
+export const threshold = 1_600_000
 
 export const instantiations = 12_000
 
