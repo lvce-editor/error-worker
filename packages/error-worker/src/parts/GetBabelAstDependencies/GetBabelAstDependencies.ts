e3b08690ab70f8075d858da13db653864b4d5520
Le Vivilet <levivilet@pm.me>
2026-04-22 22:07:41 +0200

    fix

--- packages/error-worker/src/parts/GetBabelAstDependencies/GetBabelAstDependencies.ts
+++ packages/error-worker/src/parts/GetBabelAstDependencies/GetBabelAstDependencies.ts
@@ -1,5 +1,5 @@
 import type * as BabelParser from '@babel/parser'
-import { type File } from '@babel/types'
+import type { File } from '@babel/types'
 import type { Dependency } from '../Dependency/Dependency.ts'
 import { visitNode } from '../VisitNode/VisitNode.ts'
 import * as Walk from '../Walk/Walk.ts'
